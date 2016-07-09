6b2abbbf969dfba031b7f3539f25b282da0cd707
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-07-09 20:24:57 +0200

    More header style

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 $(function() {
 
-    // var $searchButton = $('#btn-search');
-    // $searchButton.hover(function() {
-    //     $('header section div form input').css('width', '190');
-    // });
-
+    
 
 }());
