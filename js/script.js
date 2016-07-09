bc96580b2cbf1950cbe72948fb903a779a481738
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-07-09 22:24:04 +0200

    Again Header Style

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-$(function() {
+(function() {
 
-    
+    var searchButton = $('#search-btn');
+    searchButton.on('click', function() {
+        $('header section div form').toggleClass('toggle');
+    });
+
+    $('header nav ul li').on('click', function() {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
 
 }());
