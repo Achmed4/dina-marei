d44d359b90c21b1d68053827ca564497f013459a
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-07-10 15:10:57 +0200

    Done With Header Style

--- js/script.js
+++ js/script.js
@@ -9,4 +9,11 @@
         $(this).addClass('active').siblings().removeClass('active');
     });
 
+    $('header main nav > button').on('click', function() {
+        $('header main nav > ul').fadeIn('300');
+    });
+    $('header main nav > ul span').on('click', function() {
+        $('header main nav > ul').fadeOut('300');
+    });
+
 }());
