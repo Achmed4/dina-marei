672f9edd48f202006b59f29b7533fa1878514fa1
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-07-20 11:39:22 +0200

    Added Product Page Files

---
+++ js/productSlider.js
@@ -0,0 +1,11 @@
+(function() {
+
+    $('#product-slider').slick({
+      infinite: true,
+      prevArrow: "<i class='fa fa-angle-right'></i>",
+      nextArrow: "<i class='fa fa-angle-left'></i>",
+      slidesToShow: 3,
+      slidesToScroll: 3
+    });
+
+}());
