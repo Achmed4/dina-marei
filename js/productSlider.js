c6e5e2fd8372a4c70e5969fc0eef0b66586ed1da
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-07-26 14:12:47 +0200

    Added Product Zoom

--- js/productSlider.js
+++ js/productSlider.js
@@ -8,4 +8,24 @@
       slidesToScroll: 1,
     });
 
+    //Initiate ZOOM Plugin
+    // var dataZoomImage = $('#zoom_01').data('zoom-image');
+    $("#zoom_01 img").elevateZoom({
+        zoomType: "inner",
+        cursor: "crosshair"
+    });
+
+    // $('#product-slider img').on('click', function() {
+    //     console.log('dataZoomImage');
+    // });
+
+    var imagePath, newDataZoomImage;
+
+    $('#product-slider .image-parent').on('click', function() {
+        // newDataZoomImage = $(this).children().data('zoom-image');
+        imagePath = $(this).html();
+        $('.col #zoom_01').html(imagePath).elevateZoom();
+        console.log(imagePath);
+    });
+
 }());
