36d8473d5cb4257dd7b69c6d6b991c8e49925415
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-07-26 16:07:38 +0200

    Done With Product Page

--- js/productSlider.js
+++ js/productSlider.js
@@ -8,10 +8,21 @@
       slidesToScroll: 1,
     });
 
+    $(window).resize(function() {
+        $('#product-slider').slick({
+          infinite: true,
+          prevArrow: "<i class='fa fa-angle-left'></i>",
+          nextArrow: "<i class='fa fa-angle-right'></i>",
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        });
+    });
+
     //Initiate ZOOM Plugin
     $("#zoom_01 img").elevateZoom({
         zoomType: "inner",
-        cursor: "crosshair"
+        cursor: "crosshair",
+        responsive: true,
     });
 
     var imagePath;
@@ -19,7 +30,8 @@
         imagePath = $(this).html();
         $('#zoom_01').html(imagePath).children().elevateZoom({
             zoomType: "inner",
-            cursor: "crosshair"
+            cursor: "crosshair",
+            responsive: true,
         });
         console.log(imagePath);
     });
