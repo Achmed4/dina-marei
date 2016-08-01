d7616b5768498818d19b94a37a8e91042dc4fe04
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-08-01 13:57:11 +0200

    Added Currency Select to the top-bar

--- js/script.js
+++ js/script.js
@@ -5,11 +5,6 @@
         $('header > section .container div > form').toggleClass('toggle');
     });
 
-    var socialButton = $('#social-btn');
-    socialButton.on('click', function() {
-        $('header > section .container div > ul').toggleClass('toggle');
-    });
-
     // Adding Active Class to Nav Links
     $('header nav ul li').on('click', function() {
         $(this).addClass('active').siblings().removeClass('active');
@@ -33,30 +28,18 @@
     });
 
     // Adjusting Nav Menu on Mobile
-    // $('header main nav > button').on('click', function() {
-    //     $('header main nav > div:last-child ul').fadeIn('300');
-    //     $('body').css('overflow', 'hidden');
-    // });
-    // $('header main nav > ul span').on('click', function() {
-    //     $('header main nav > div:last-child ul').fadeOut('300');
-    //     $('body').css('overflow-y', 'visible');
-    // });
-
     function openNav() {
         $("header main nav > div:last-child").css('width', '100%');
         $('body').css('overflow', 'hidden');
     }
-
     function closeNav() {
         $("header main nav > div:last-child").css('width', '0%');
         $('body').css('overflow', 'auto');
     }
-
     /* Open when someone clicks on the Hamburger button */
     $('#hamburger').on('click', function() {
         openNav();
     });
-
     /* Close when someone clicks on the "x" symbol inside the mobile menu */
     $('.closebtn').on('click', function() {
         closeNav();
